bfbce7d6dda14f154884d1554f07e309ab794038
Kazuya Ueoka <k.ueoka@timers-inc.com>
2016-06-30 15:15:56 +0900

    Initial Commit

---
+++ HashableDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HashableDemo
+//
+//  Created by Kazuya Ueoka on 2016/06/30.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
