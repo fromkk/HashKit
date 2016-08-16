7b2092477fab121b2ca96df53ee5fd7f69d19c5f
Kazuya Ueoka <k.ueoka@timers-inc.com>
2016-08-16 14:40:45 +0900

    add watch target

---
+++ HashKitWatch/HashKitWatch.h
@@ -0,0 +1,19 @@
+//
+//  HashKitWatch.h
+//  HashKitWatch
+//
+//  Created by Kazuya Ueoka on 2016/08/16.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for HashKitWatch.
+FOUNDATION_EXPORT double HashKitWatchVersionNumber;
+
+//! Project version string for HashKitWatch.
+FOUNDATION_EXPORT const unsigned char HashKitWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HashKitWatch/PublicHeader.h>
+
+
