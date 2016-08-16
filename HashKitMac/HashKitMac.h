36516cd61b6b34683d8ccc094f83797e4e491993
Kazuya Ueoka <k.ueoka@timers-inc.com>
2016-08-16 14:28:05 +0900

    add multi targets

---
+++ HashKitMac/HashKitMac.h
@@ -0,0 +1,19 @@
+//
+//  HashKitMac.h
+//  HashKitMac
+//
+//  Created by Kazuya Ueoka on 2016/08/16.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HashKitMac.
+FOUNDATION_EXPORT double HashKitMacVersionNumber;
+
+//! Project version string for HashKitMac.
+FOUNDATION_EXPORT const unsigned char HashKitMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HashKitMac/PublicHeader.h>
+
+
---
+++ HashKitTv/HashKitTv.h
@@ -0,0 +1,19 @@
+//
+//  HashKitTv.h
+//  HashKitTv
+//
+//  Created by Kazuya Ueoka on 2016/08/16.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HashKitTv.
+FOUNDATION_EXPORT double HashKitTvVersionNumber;
+
+//! Project version string for HashKitTv.
+FOUNDATION_EXPORT const unsigned char HashKitTvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HashKitTv/PublicHeader.h>
+
+
