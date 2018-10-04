041385fdc877cbbfa7662d4044867cf4a04856f3
Kazuya Ueoka <k.ueoka@timers-inc.com>
2018-10-04 11:23:39 +0900

    update to Swift 4.2

--- Sources/HashKit.h
+++ Sources/HashKit.h
@@ -5,5 +5,3 @@
 //  Created by Kazuya Ueoka on 2016/11/02.
 //  Copyright © 2016年 fromKK. All rights reserved.
 //
-
-#import <CommonCrypto/CommonCrypto.h>
--- Sources/HashKit.swift
+++ Sources/HashKit.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CommonCrypto
 
 public enum HashableType {
     case md5
